5b99c67fd99b40c0273d1121dc7f0a06246c25a2
Joven Usaraga <jovenusaraga09@gmail.com>
2026-08-14 18:13:08 +0800

    Pygame founding

---
+++ 01_window.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640,480))
+pygame.display.set_caption("My first Pygame Window")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
